a4698fecf37a6d4d7a82ceb802273644087e3ba3
Matias Deveze <matiasdeveze@etu.unilim.fr>
2023-04-06 17:29:50 +0200

    change Arbre et MainLeVerger

--- src/main/java/lerverger/model/Arbre.java
+++ src/main/java/lerverger/model/Arbre.java
@@ -1,5 +1,32 @@
 package lerverger.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Arbre {
-	//TODO faire une collection d'arbre
+	protected  ArrayList<Fruit> arbre;
+	private Couleur couleur;
+
+	public Arbre(ArrayList<Fruit> arbre,Couleur couleur) {
+		this.arbre = arbre;
+		this.couleur = couleur;
+	}
+	
+	public void remplir() {
+		int nombreDeFruit = 10;
+		for (int compteur=0 ; compteur <= nombreDeFruit ; compteur++) {
+			this.arbre.add(new Fruit(this.couleur));
+		}
+	}
+	
+	public void enlever() {
+		//TODO Pas terminer 
+		if (arbre.size()==0) {
+			//TODO Message d'erreur
+		}
+		else {
+			private Fruit fruit = arbre.get(0);
+			this.arbre.remove(fruit);
+		}
+	}
 }
--- src/main/java/leverger/application/MainLeVerger.java
+++ src/main/java/leverger/application/MainLeVerger.java
@@ -1,10 +1,11 @@
 package leverger.application;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class MainLeVerger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//TODO
 	}
-
 }
