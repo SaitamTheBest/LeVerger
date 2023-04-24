ce29a2decdbe198b80e4af3cc5c5c9605514fa4d
Matias Deveze <matiasdeveze@etu.unilim.fr>
2023-04-24 14:42:17 +0200

    finition de Arbre

--- src/main/java/lerverger/model/Arbre.java
+++ src/main/java/lerverger/model/Arbre.java
@@ -1,32 +1,39 @@
 package lerverger.model;
 
+import java.util.List;
 import java.util.ArrayList;
-import java.util.Collection;
 
 public class Arbre {
-	protected  ArrayList<Fruit> arbre;
+	protected List<Fruit> arbre;
 	private Couleur couleur;
 
-	public Arbre(ArrayList<Fruit> arbre,Couleur couleur) {
+	public Arbre(List<Fruit> arbre, Couleur couleur) {
 		this.arbre = arbre;
 		this.couleur = couleur;
 	}
-	
+
+	public Arbre(Couleur couleur) {
+		this(new ArrayList<>(),couleur);
+	}
+
 	public void remplir() {
 		int nombreDeFruit = 10;
-		for (int compteur=0 ; compteur <= nombreDeFruit ; compteur++) {
+		for (int compteur = 0; compteur <= nombreDeFruit-1; compteur++) {
 			this.arbre.add(new Fruit(this.couleur));
 		}
 	}
-	
+
 	public void enlever() {
-		//TODO Pas terminer 
-		if (arbre.size()==0) {
-			//TODO Message d'erreur
-		}
-		else {
-			private Fruit fruit = arbre.get(0);
-			this.arbre.remove(fruit);
+		if (!arbre.isEmpty()) {
+			Fruit fruit = arbre.get(0);
+			arbre.remove(fruit);
 		}
 	}
+
+	@Override
+	public String toString() {
+		return "Arbre [arbre=" + arbre.size() + ", couleur=" + couleur + "]";
+	}
+	
+	
 }
