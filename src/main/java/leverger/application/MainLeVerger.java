31c7e721e3e34335976b0185a8d93cd985eeec69
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-05-03 11:50:41 +0200

    ajout Image, Packages, Modification de l'interface IHM

--- src/main/java/leverger/application/MainLeVerger.java
+++ src/main/java/leverger/application/MainLeVerger.java
@@ -4,22 +4,35 @@ import javafx.scene.control.CheckBox;
 
 import javafx.scene.input.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.event.*;
+import javafx.scene.image.Image;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import leverger.model.Arbre;
@@ -30,8 +43,6 @@ public class MainLeVerger extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// Nombre de tour
-		int nombreDeTour = 0;
 
 		// Création Arbre
 		Arbre arbreJaune = new Arbre(Couleur.JAUNE);
@@ -49,21 +60,21 @@ public class MainLeVerger extends Application {
 		De deJoueur = new De();
 
 		// Label Application
-		Label nombreDeTourLabel = new Label("Manche " + nombreDeTour);
+		Label nombreDeTourLabel = new Label("Manche " + deJoueur.nombreDeTour());
 		Label nombreDeFruitJauneLabel = new Label(arbreJaune.nombreDeFruit() + " fruit(s)");
 		Label nombreDeFruitRougeLabel = new Label(arbreRouge.nombreDeFruit() + " fruit(s)");
 		Label nombreDeFruitVertLabel = new Label(arbreVert.nombreDeFruit() + " fruit(s)");
 		Label nombreDeFruitBleuLabel = new Label(arbreBleu.nombreDeFruit() + " fruit(s)");
-		Label faceDuDeLabel = new Label("None");
 
 		// Boutons
 		Button lancerBouton = new Button("Lancer");
 
-		// Alerts
-
-		Alert alertGagner = new Alert(AlertType.INFORMATION);
-		alertGagner.setTitle("Résultat du jeu");
-		alertGagner.setHeaderText("Vous avez gagné !");
+		// Canvas
+		Circle circle = new Circle();
+		circle.setFill(Color.TRANSPARENT);
+		circle.setCenterX(50);
+		circle.setCenterY(50);
+		circle.setRadius(25);
 
 		// Events
 
@@ -71,29 +82,38 @@ public class MainLeVerger extends Application {
 
 			@Override
 			public void handle(MouseEvent arg0) {
+
 				Couleur faceDuDe = deJoueur.lancer();
-				faceDuDeLabel.setText("" + faceDuDe);
-				//TODO Il manque l'affichage de manches et des couleurs des fruits
+				nombreDeTourLabel.setText("Manche " + deJoueur.nombreDeTour());
 
 				if (faceDuDe.equals(arbreBleu.getCouleur())) {
 					arbreBleu.enlever();
 					nombreDeFruitBleuLabel.setText(arbreBleu.nombreDeFruit() + " fruit(s)");
+					circle.setFill(Color.BLUE);
+
 				}
 				if (faceDuDe.equals(arbreJaune.getCouleur())) {
 					arbreJaune.enlever();
 					nombreDeFruitJauneLabel.setText(arbreJaune.nombreDeFruit() + " fruit(s)");
+					circle.setFill(Color.YELLOW);
 				}
 				if (faceDuDe.equals(arbreRouge.getCouleur())) {
 					arbreRouge.enlever();
 					nombreDeFruitRougeLabel.setText(arbreRouge.nombreDeFruit() + " fruit(s)");
+					circle.setFill(Color.RED);
 				}
 				if (faceDuDe.equals(arbreVert.getCouleur())) {
 					arbreVert.enlever();
 					nombreDeFruitVertLabel.setText(arbreVert.nombreDeFruit() + " fruit(s)");
+					circle.setFill(Color.GREEN);
 				}
 
 				if (arbreBleu.nombreDeFruit() == 0 && arbreJaune.nombreDeFruit() == 0 && arbreRouge.nombreDeFruit() == 0
 						&& arbreVert.nombreDeFruit() == 0) {
+					// Alerts gagnant
+					Alert alertGagner = new Alert(AlertType.INFORMATION);
+					alertGagner.setTitle("Résultat du jeu");
+					alertGagner.setHeaderText("Vous avez gagné en " + deJoueur.nombreDeTour() + " manches !");
 					alertGagner.showAndWait();
 					lancerBouton.setDisable(true);
 				}
@@ -108,12 +128,17 @@ public class MainLeVerger extends Application {
 		root.add(nombreDeFruitJauneLabel, 3, 0);
 		root.add(lancerBouton, 0, 1);
 		root.add(nombreDeTourLabel, 3, 1);
-		root.add(faceDuDeLabel, 2, 1);
+		root.add(circle, 2, 1);
 
-		root.setHgap(10);
-		root.setVgap(10);
+		root.setHgap(20);
+		root.setVgap(20);
 		root.setAlignment(Pos.CENTER);
 
+		File fondFichier = new File("image/leverger-fond-image.jpep");
+		Image fondImage = new Image(new FileInputStream(fondFichier));
+		BackgroundImage backgroundImage = new BackgroundImage(fondImage, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+		root.setBackground(new Background(backgroundImage));
+		
 		Scene scene = new Scene(root, 900, 600);
 		stage.setScene(scene);
 		stage.setTitle("Le Verger");
