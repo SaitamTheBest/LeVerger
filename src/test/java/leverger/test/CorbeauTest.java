e1838afe53be91248837f12eacbded5fe66a3984
Matias DEVEZE <matias.deveze@etu.unilim.fr>
2023-05-24 14:26:56 +0200

    ajout de tests pour CorbeauTest

--- src/test/java/leverger/test/CorbeauTest.java
+++ src/test/java/leverger/test/CorbeauTest.java
@@ -3,52 +3,65 @@ package leverger.test;
 import static org.junit.jupiter.api.Assertions.*;
 import leverger.model.Corbeau;
 
-
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 class CorbeauTest {
-	Corbeau corbeau = new Corbeau();
+	private static final Color DEVOILE_PIECE = Color.TRANSPARENT;
+	private Corbeau corbeau;
+	
+	@BeforeEach
+	void initialisation() {
+		List<Rectangle> listeRectangle = new ArrayList<>();
+		for (int compteur = 0;compteur<9;compteur++) {
+			listeRectangle.add(new Rectangle());
+		}
+		corbeau = new Corbeau(listeRectangle);
+	}
 	
 	@Test
 	void testCreationCorbeau() {
-		assertEquals(9, corbeau.taille());
+		int resultat = corbeau.taille();
+		assertEquals(9, resultat);
 	}
 	@Test
 	void testRecuperationListe() {
-		List<Rectangle> faceCorbeau = corbeau.liste();
-		assertTrue(faceCorbeau instanceof List<Boolean>);
+		List <Rectangle> resultat = corbeau.retourneListe();
+		assertTrue(resultat instanceof List<Rectangle>);
 	}
 	
 	@Test
 	void testDevoilement() {
 		corbeau.devoile();
-		List<Rectangle> faceCorbeau = corbeau.liste();
-		Boolean retourne = false;
-		for (Boolean bool : faceCorbeau) {
-			if (bool) {
-				retourne = true;
+		List<Rectangle> faceCorbeau = corbeau.retourneListe();
+		Boolean pieceRetourner = false;
+		for (Rectangle piece : faceCorbeau) {
+			if (piece.getFill().equals(DEVOILE_PIECE)) {
+				pieceRetourner = true;
 			}
 		}
-		assertTrue(retourne);
+		assertTrue(pieceRetourner);
 	}
 	
 	@Test
-	void testreinitialisation() {
+	void testReinitialisation() {
 		corbeau.devoile();
-		List <Boolean> faceCorbeau = corbeau.liste();
+		List <Rectangle> faceCorbeau = corbeau.retourneListe();
 		corbeau.reinitialiser();
-		Boolean retourne = false;
-		for (Boolean bool : faceCorbeau) {
-			if (bool) {
-				retourne = true;
+		Boolean pieceRetourner = false;
+		for (Rectangle piece : faceCorbeau) {
+			if (piece.getFill().equals(DEVOILE_PIECE)) {
+				pieceRetourner = true;
 			}
 		}
-		assertFalse(retourne);
+		assertFalse(pieceRetourner);
 	}
 
 }
